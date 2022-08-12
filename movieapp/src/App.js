598863b360cfe68ca4adc24a1584fb33ec26cee6
hyoseung <ace.aaiden@gmail.com>
2022-08-12 12:57:26 +0900

    라우터세팅

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
-  return <div>여기서 시작</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
